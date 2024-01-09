e278738f76e25c6e7a71cb90b8d213f75be71169
MahdiarMansouri <mahdiarm60@gmail.com>
2024-01-09 23:34:51 +0330

    Add jupyter notebook / torch based

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from PIL import Image
 
 # Downloading Data
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
-
+# (X_train, y_train), (X_test, y_test) = (X_train[:1000], y_train[:1000]), (X_test[:200], y_test[:200])
 # HOG feature extraction
 X_train_hog = []
 X_test_hog = []
@@ -23,18 +23,21 @@ X_test_combined = []
 
 for idx, img in enumerate(X_train):
     img_hog = []
+
     for channel_num in range(img.shape[2]):
         _, hog_channel = hog(img[:, :, channel_num], pixels_per_cell=(8, 8), cells_per_block=(2, 2),
                              block_norm='L2-Hys', visualize=True)
         hog_channel = exposure.rescale_intensity(hog_channel, in_range=(0, 10))
         img_hog.append(hog_channel)
+    # print(len(img_hog))
     img_hog = np.dstack((np.array(img_hog[0]), np.array(img_hog[1]), np.array(img_hog[2])))
     X_train_hog.append(img_hog)
-    img_combined = np.concatenate((img[None, :, :, :], img_hog[None, :, :, :]))
+    img_combined = np.dstack((img, img_hog))
+    # img_combined = np.concatenate((img[None, :, :, :], img_hog[None, :, :, :]))
     X_train_combined.append(img_combined)
-    print(img.shape)
-    print(img_hog.shape)
-    print(img_combined.shape)
+    # print(img.shape)
+    # print(img_hog.shape)
+    # print(img_combined.shape)
 
 print('Train dataset is Done!')
 
@@ -48,7 +51,8 @@ for idx, img in enumerate(X_test):
         img_hog.append(hog_channel)
     img_hog = np.dstack((np.array(img_hog[0]), np.array(img_hog[1]), np.array(img_hog[2])))
     X_test_hog.append(img_hog)
-    img_combined = np.concatenate((img[None, :, :, :], img_hog[None, :, :, :]))
+    img_combined = np.dstack((img, img_hog))
+    # img_combined = np.concatenate((img[None, :, :, :], img_hog[None, :, :, :]))
     X_test_combined.append(img_combined)
 
 print('Test dataset is Done!')
@@ -59,7 +63,7 @@ print('HOG feature extraction Done!..')
 # Building Model
 model = Sequential()
 
-model.add(Conv2D(64, input_shape=(2, 32, 32, 3), kernel_size=(3, 3), padding='same', activation='relu'))
+model.add(Conv2D(64, input_shape=(32, 32, 6), kernel_size=(3, 3), padding='same', activation='relu'))
 model.add(MaxPool2D(pool_size=(2, 2)))
 
 model.add(Conv2D(64, kernel_size=(3, 3), padding='same', activation='relu'))
@@ -75,29 +79,32 @@ model.add(Dense(32, activation='relu'))
 model.add(Dense(10, activation='softmax'))
 
 model.compile(optimizer=Adam(learning_rate=0.001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
+print(model.summary())
 print('Model built!..')
 
+# print(np.array(X_train_combined).shape)
 # Train model
 history = model.fit(
-    X_train_combined,
+    np.array(X_train_combined),
     y_train,
+    batch_size=64,
     validation_split=0.1,
-    epochs=30,
+    epochs=100,
 )
 
 # Evaluate model
-print(model.evaluate(X_test_combined, y_test))
+loss, accuracy = model.evaluate(np.array(X_test_combined), y_test)
+print(f'The Loss is {loss} and Accuracy is {accuracy}')
 
 # Plot the results
 plt.subplot(1, 2, 1)
 plt.title('Accuracy')
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
+plt.plot(history.history['accuracy'], label='train')
+plt.plot(history.history['val_accuracy'], label='validation')
 
 plt.subplot(1, 2, 2)
 plt.title('Loss')
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='validation')
+plt.legend()
 plt.show()
